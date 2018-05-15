b5cb64b88553e549ebb79b7ae2575f92f17254ac
Simon Praetorius <praetorius@sitegeist.de>
2018-05-15 19:47:11 +0200

    Initial commit

---
+++ ext_emconf.php
@@ -0,0 +1,43 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "sms_responsive_images".
+#
+# Auto generated 05-05-2014 09:20
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+    'title' => 'Fluid Components',
+    'description' => '',
+    'category' => 'fe',
+    'author' => 'Simon Praetorius',
+    'author_email' => 'praetorius@sitegeist.de',
+    'author_company' => 'sitegeist media solutions GmbH',
+    'shy' => '',
+    'priority' => '',
+    'module' => '',
+    'state' => 'beta',
+    'internal' => '',
+    'uploadfolder' => false,
+    'createDirs' => '',
+    'modify_tables' => '',
+    'clearCacheOnLoad' => false,
+    'lockType' => '',
+    'version' => '1.0.0',
+    'constraints' => array(
+        'depends' => array(
+            'typo3' => '8.7.0-8.7.99',
+            'fluid' => '',
+            'php' => '7.0.0-0.0.0'
+        ),
+        'conflicts' => array(
+        ),
+        'suggests' => array(
+            'fluid_styled_content' => ''
+        ),
+    )
+);
