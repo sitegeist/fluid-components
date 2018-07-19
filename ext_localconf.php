810d952c401d37c9c7f2844c9b6bfab3d99deefc
Simon Praetorius <praetorius@sitegeist.de>
2018-07-19 13:34:11 +0200

    [TASK] Use new array syntax

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,9 +1,9 @@
 <?php
 
 call_user_func(function () {
-    $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\TYPO3\CMS\Fluid\Core\ViewHelper\ViewHelperResolver::class] = array(
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\TYPO3\CMS\Fluid\Core\ViewHelper\ViewHelperResolver::class] = [
         'className' => \SMS\FluidComponents\Fluid\ViewHelperResolver::class
-    );
+    ];
 
     // Make fc a global namespace
     if (!isset($GLOBALS['TYPO3_CONF_VARS']['SYS']['fluid']['namespaces']['fc'])) {
