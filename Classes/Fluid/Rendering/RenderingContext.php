561c7bc9f0a399b5729adbc2ba6fe7539fc5a961
Simon Praetorius <praetorius@sitegeist.de>
2018-08-13 09:26:28 +0200

    [TASK] Fix constructor of renderingcontext

--- Classes/Fluid/Rendering/RenderingContext.php
+++ Classes/Fluid/Rendering/RenderingContext.php
@@ -3,12 +3,13 @@
 namespace SMS\FluidComponents\Fluid\Rendering;
 
 use SMS\FluidComponents\Fluid\Compiler\NodeConverter;
+use TYPO3Fluid\Fluid\View\ViewInterface;
 
 class RenderingContext extends \TYPO3\CMS\Fluid\Core\Rendering\RenderingContext
 {
-    public function __construct()
+    public function __construct(ViewInterface $view = null)
     {
-        parent::__construct();
+        parent::__construct($view);
 
         // Use custom node converter to be able to use dynamic default values
         // for component params
