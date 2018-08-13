951caccb3e3e041ed42518fa1c71d2e64a52594d
Simon Praetorius <praetorius@sitegeist.de>
2018-08-13 09:26:40 +0200

    [TASK] Compatibility to TYPO3 9

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,15 +1,4 @@
 <?php
-
-########################################################################
-# Extension Manager/Repository config file for ext "sms_responsive_images".
-#
-# Auto generated 05-05-2014 09:20
-#
-# Manual updates:
-# Only the data in the array - everything else is removed by next
-# writing. "version" and "dependencies" must not be touched!
-########################################################################
-
 $EM_CONF[$_EXTKEY] = array(
     'title' => 'Fluid Components',
     'description' => '',
@@ -30,14 +19,13 @@ $EM_CONF[$_EXTKEY] = array(
     'version' => '1.0.0',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '8.7.0-8.7.99',
+            'typo3' => '8.7.0-9.9.99',
             'fluid' => '',
             'php' => '7.0.0-0.0.0'
         ),
         'conflicts' => array(
         ),
         'suggests' => array(
-            'fluid_styled_content' => ''
         ),
     )
 );
