157706824dabb6b2aabee670cd3d3c103c77efdd
Simon Praetorius <praetorius@sitegeist.de>
2020-03-13 13:40:42 +0100

    [FEATURE] Data structure to represent TYPO3 navigations

---
+++ Classes/Domain/Model/Navigation.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace SMS\FluidComponents\Domain\Model;
+
+use SMS\FluidComponents\Interfaces\ConstructibleFromArray;
+use SMS\FluidComponents\Domain\Model\NavigationItem;
+
+/**
+ * Data Structure to generate a navigation in components
+ */
+class Navigation implements \Iterator, \Countable, ConstructibleFromArray
+{
+    /**
+     * Navigation items
+     *
+     * @var NavigationItem[]
+     */
+    protected $items = [];
+
+    /**
+     * Initializes a navigation object from a TYPO3 navigation array
+     *
+     * @param array $items
+     */
+    public function __construct(array $items)
+    {
+        $this->setItems($items);
+    }
+
+    /**
+     * Initializes a navigation object from a TYPO3 navigation array
+     *
+     * @param array $items
+     */
+    public static function fromArray(array $items): self
+    {
+        return new static($items);
+    }
+
+    public function getItems(): array
+    {
+        return $this->items;
+    }
+
+    public function setItems(array $items): self
+    {
+        // Make sure that navigation items use the appropriate data structure
+        $this->items = array_filter(array_map([$this, 'sanitizeNavigationItem'], $items));
+        return $this;
+    }
+
+    public function count(): int
+    {
+        return count($this->items);
+    }
+
+    public function current()
+    {
+        return current($this->items);
+    }
+
+    public function key()
+    {
+        return key($this->items);
+    }
+
+    public function next(): void
+    {
+        next($this->items);
+    }
+
+    public function rewind(): void
+    {
+        reset($this->items);
+    }
+
+    public function valid(): bool
+    {
+        return $this->current() !== false;
+    }
+
+    /**
+     * Makes sure that the provided item is a valid data structure
+     *
+     * @param mixed $item
+     * @return NavigationItem|null
+     */
+    protected function sanitizeNavigationItem($item): ?NavigationItem
+    {
+        if ($item instanceof NavigationItem) {
+            return $item;
+        }
+
+        if (is_array($item)) {
+            return NavigationItem::fromArray($item);
+        }
+
+        return null;
+    }
+}
---
+++ Classes/Domain/Model/NavigationItem.php
@@ -0,0 +1,224 @@
+<?php
+
+namespace SMS\FluidComponents\Domain\Model;
+
+use SMS\FluidComponents\Domain\Model\Typolink;
+use SMS\FluidComponents\Domain\Model\Navigation;
+use SMS\FluidComponents\Interfaces\ConstructibleFromArray;
+
+/**
+ * Data Structure to represent one item in a navigation
+ */
+class NavigationItem implements ConstructibleFromArray
+{
+    /**
+     * Title of the navigation item
+     *
+     * @var string
+     */
+    protected $title;
+
+    /**
+     * Link of the navigation item
+     *
+     * @var Typolink
+     */
+    protected $link;
+
+    /**
+     * Indicates whether the navigation item is part of the current rootline
+     * (parent page of the current page OR current page)
+     *
+     * @var boolean
+     */
+    protected $active = false;
+
+    /**
+     * Indicates whether the navigation item represents the current page
+     * (note that active will be true as well)
+     *
+     * @var boolean
+     */
+    protected $current = false;
+
+    /**
+     * Indicates whether the navigation item is a spacer item without real
+     * functionality
+     *
+     * @var boolean
+     */
+    protected $spacer = false;
+
+    /**
+     * Submenu of the navigation item
+     *
+     * @var Navigation
+     */
+    protected $children;
+
+    /**
+     * Raw data of the navigation item, e. g. the pages record
+     *
+     * @var array
+     */
+    protected $data = [];
+
+    /**
+     * Creates a navigation item object
+     *
+     * @param string $title         title of the item
+     * @param Typolink $link        link of the item
+     * @param boolean $current      true if item represents the current page
+     * @param boolean $active       true if item is part of current rootline
+     * @param boolean $spacer       true if item is a spacer item
+     * @param Navigation $children  sub navigation
+     * @param array $data           raw item data
+     */
+    public function __construct(
+        string $title,
+        Typolink $link = null,
+        bool $current = false,
+        bool $active = false,
+        bool $spacer = false,
+        Navigation $children = null,
+        array $data = []
+    ) {
+        // Ensure valid data structures for optional values
+        if (!$link) {
+            $link = new Typolink('');
+        }
+        if (!$children) {
+            $children = new Navigation([]);
+        }
+
+        $this
+            ->setTitle($title)
+            ->setLink($link)
+            ->setCurrent($current)
+            ->setActive($active)
+            ->setSpacer($spacer)
+            ->setChildren($children)
+            ->setData($data);
+    }
+
+    /**
+     * Creates a navigation item object based on a TYPO3 navigation item array
+     *
+     * @param array $navigationItem  respected properties that will become part of the data structure:
+     *                               title, link, current, active, spacer, children, data
+     * @return self
+     */
+    public static function fromArray(array $navigationItem): self
+    {
+        // Convert link and sub navigation to the appropriate data structure
+        if (isset($navigationItem['link']) && !$navigationItem['link'] instanceof Typolink) {
+            $navigationItem['link'] = new Typolink($navigationItem['link']);
+        }
+        if (isset($navigationItem['children']) && !$navigationItem['children'] instanceof Navigation) {
+            $navigationItem['children'] = new Navigation($navigationItem['children']);
+        }
+
+        return new static(
+            $navigationItem['title'] ?? '',
+            $navigationItem['link'] ?? null,
+            $navigationItem['current'] ?? false,
+            $navigationItem['active'] ?? false,
+            $navigationItem['spacer'] ?? false,
+            $navigationItem['children'] ?? null,
+            $navigationItem['data'] ?? []
+        );
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+        return $this;
+    }
+
+    public function getLink(): ?Typolink
+    {
+        return $this->link;
+    }
+
+    public function setLink(Typolink $link): self
+    {
+        $this->link = $link;
+        return $this;
+    }
+
+    public function getTarget(): string
+    {
+        return $this->link->getTarget();
+    }
+
+    public function setTarget(string $target): self
+    {
+        $this->link->setTarget($target);
+        return $this;
+    }
+
+    public function getCurrent(): bool
+    {
+        return $this->current;
+    }
+
+    public function setCurrent(bool $current): self
+    {
+        $this->current = $current;
+        return $this;
+    }
+
+    public function getActive(): bool
+    {
+        return $this->active;
+    }
+
+    public function setActive(bool $active): self
+    {
+        $this->active = $active;
+        return $this;
+    }
+
+    public function getSpacer(): bool
+    {
+        return $this->spacer;
+    }
+
+    public function setSpacer(bool $spacer): self
+    {
+        $this->spacer = $spacer;
+        return $this;
+    }
+
+    public function getChildren(): Navigation
+    {
+        return $this->children;
+    }
+
+    public function hasChildren(): bool
+    {
+        return count($this->children) > 0;
+    }
+
+    public function setChildren(Navigation $children): self
+    {
+        $this->children = $children;
+        return $this;
+    }
+
+    public function getData(): array
+    {
+        return $this->data;
+    }
+
+    public function setData(array $data): self
+    {
+        $this->data = $data;
+        return $this;
+    }
+}
