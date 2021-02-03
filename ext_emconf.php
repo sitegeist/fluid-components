3b76bf1459584264577aa39fc5215f3646cb85c9
Simon Praetorius <praetorius@sitegeist.de>
2021-02-03 18:32:05 +0100

    [TASK] Fix version constraint

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@ $EM_CONF['fluid_components'] = [
     'version' => '2.4.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-10.9.99',
+            'typo3' => '9.5.0-11.9.99',
             'php' => '7.2.0-7.9.99'
         ],
         'conflicts' => [
