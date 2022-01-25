151a66895daf5e94b6e2d933bd3c9f794a22f2fb
Benjamin Tammling <atomschinken@gmail.com>
2022-01-25 18:04:22 +0100

    [TASK] Refactor testing
    * [TASK] Refactor testing
    
    - Add a helper script to start the tests
    - Change integration to functional tests
    - Use docker for testing
    
    * Fix errors in test configuration
    
    * Reactivate functional tests
    
    * Fix error in github workflow configuration file
    
    Co-authored-by: Benjamin Tammling <benjamin.tammling@sitegeist.de>
    Co-authored-by: Simon Praetorius <praetorius@sitegeist.de>

---
+++ Tests/Functional/ComponentRendererTest.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace SMS\FluidComponents\Tests\Functional;
+
+use SMS\FluidComponents\Fluid\ViewHelper\ComponentRenderer;
+use TYPO3\CMS\Core\Cache\Backend\NullBackend;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Mvc\Request;
+use TYPO3\CMS\Fluid\Core\Rendering\RenderingContext;
+use TYPO3\TestingFramework\Core\Functional\FunctionalTestCase;
+use TYPO3Fluid\Fluid\Core\ViewHelper\ViewHelperInvoker;
+
+class ComponentRendererTest extends FunctionalTestCase
+{
+    protected $fluidNamespaces;
+    protected $componentNamespaces;
+    protected $testNamespace = 'SMS\\FluidComponents\\Tests\\Fixtures\\Functional\\Components';
+    protected $resetSingletonInstances = true;
+
+    protected $initializeDatabase = false;
+    protected $testExtensionsToLoad = [
+        'typo3conf/ext/fluid_components'
+    ];
+
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        // Add fc ViewHelpers
+        $this->fluidNamespaces = $GLOBALS['TYPO3_CONF_VARS']['SYS']['fluid']['namespaces'] ?? null;
+        $GLOBALS['TYPO3_CONF_VARS']['SYS']['fluid']['namespaces']['fc'][] = 'SMS\\FluidComponents\\ViewHelpers';
+
+        // Register test components
+        $this->componentNamespaces = $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['fluid_components']['namespaces'] ?? null;
+        $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['fluid_components']['namespaces'] = [
+            $this->testNamespace => realpath(dirname(__FILE__) . '/../Fixtures/Functional/Components/')
+        ];
+
+        // Register and then disable fluid cache
+        // This is necessary because of the way the RenderingContext deals with the fluid cache
+        $cacheClass = $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['fluid_template']['frontend'];
+        $cacheManager = GeneralUtility::makeInstance(\TYPO3\CMS\Core\Cache\CacheManager::class);
+        $cacheManager->registerCache(new $cacheClass('fluid_template', new NullBackend(null)));
+    }
+
+    public function renderComponentProvider()
+    {
+        return [
+            ['WithoutParameters', [], '', 'Just a renderer.'],
+            ['ContentParameter', ['content' => 'children'], '', 'children'],
+            ['ContentParameter', [], 'children', 'children'],
+            ['ScalarParameters', [
+                'stringParameter' => 'This is a string',
+                'integerParameter' => 123,
+                'trueParameter' => true, // strange fluid behavior: will be 1
+                'falseParameter' => false, // strange fluid behavior: will be empty string
+            ], '', 'This is a string|123|1|'],
+            ['DateTimeParameter', ['date' => 1601371704], '', 'Tue, 29 Sep 2020 09:28:24 +0000']
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider renderComponentProvider
+     */
+    public function renderComponent($component, $arguments, $content, $expected)
+    {
+        $container = $this->getContainer();
+
+        /** @var ComponentRenderer $renderer */
+        $renderer = $container->get(ComponentRenderer::class);
+        // Render single component
+        $renderer->setComponentNamespace($this->testNamespace . '\\' . $component);
+
+        /** @var ViewHelperInvoker $invoker */
+        $invoker = GeneralUtility::makeInstance(ViewHelperInvoker::class);
+
+        if ($container->has(RenderingContext::class)) {
+            /** @var RenderingContext $renderingContext */
+            $renderingContext = $container->get(RenderingContext::class);
+            $renderingContext->setRequest(GeneralUtility::makeInstance(Request::class));
+        } else {
+            /** @var RenderingContext $renderingContext */
+            $renderingContext = GeneralUtility::makeInstance(RenderingContext::class);
+        }
+
+        $output = $invoker->invoke(
+            $renderer,
+            $arguments,
+            $renderingContext,
+            function () use ($content) {
+                return $content;
+            }
+        );
+
+        // Ignore whitespace output from components
+        $output = trim($output);
+
+        $this->assertEquals($expected, $output);
+    }
+
+    public function tearDown(): void
+    {
+        $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['fluid_components']['namespaces'] = $this->componentNamespaces;
+        $GLOBALS['TYPO3_CONF_VARS']['SYS']['fluid']['namespaces'] = $this->fluidNamespaces;
+        parent::tearDown();
+    }
+}
