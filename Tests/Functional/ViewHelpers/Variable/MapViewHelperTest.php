e66f3cf5793c6f8f146b5683ed88fd35b0400636
Oleksandr Matsko <Oleksandr.Matsko@sitegeist.de>
2022-09-05 12:14:39 +0200

    [TASK] #71239 Change functional test for check arrays

--- Tests/Functional/ViewHelpers/Variable/MapViewHelperTest.php
+++ Tests/Functional/ViewHelpers/Variable/MapViewHelperTest.php
@@ -12,20 +12,39 @@ class MapViewHelperTest extends FunctionalTestCase
 {
     public function renderDataProvider(): \Generator
     {
-        $data[0] = [
-            'sourceSimpleField' => 'sourceSimpleValue',
-            'sourcePathField' => [
-                'path' => 'sourcePathValue'
-            ],
-            'keepField1' => 'keepValue1',
-            'keepField2' => 'keepValue2',
+        $input = [
+            0 => [
+                'sourceSimpleField' => 'sourceSimpleValue',
+                'sourcePathField' => [
+                    'path' => 'sourcePathValue'
+                ],
+                'keepField1' => 'keepValue1',
+                'keepField2' => 'keepValue2',
+            ]
         ];
 
-        yield 'tag content as value' => [
-            $data,
-            '{dataSource -> fc:variable.map(fieldMapping: {targetSimpleField: "sourceSimpleField", targetPathField: "sourcePathField.path"}, keepFields: "keepField1, keepField2") -> f:variable(name: "dataTarget")}'
-                .'{dataTarget.0.targetSimpleField}{dataTarget.0.targetPathField}{dataTarget.0.keepField1}{dataTarget.0.keepField2}',
-            $data[0]['sourceSimpleField'] . $data[0]['sourcePathField']['path'] . $data[0]['keepField1'] . $data[0]['keepField2']
+        yield 'keep only' => [
+            $input,
+            '{dataSource -> fc:variable.map(keepFields: "keepField1, keepField2") -> f:variable(name: "dataTarget")}',
+            [
+                0 => [
+                    'keepField1' => 'keepValue1',
+                    'keepField2' => 'keepValue2',
+                ]
+            ]
+        ];
+
+        yield 'full test' => [
+            $input,
+            '{dataSource -> fc:variable.map(fieldMapping: {targetSimpleField: "sourceSimpleField", targetPathField: "sourcePathField.path"}, keepFields: "keepField1, keepField2") -> f:variable(name: "dataTarget")}',
+            [
+                0 => [
+                    'targetSimpleField' => 'sourceSimpleValue',
+                    'targetPathField' => 'sourcePathValue',
+                    'keepField1' => 'keepValue1',
+                    'keepField2' => 'keepValue2',
+                ]
+            ]
         ];
     }
 
@@ -33,16 +52,17 @@ class MapViewHelperTest extends FunctionalTestCase
      * @test
      * @dataProvider renderDataProvider
      */
-    public function render(array $data, string $template, $expected): void
+    public function render(array $input, string $template, array $expected): void
     {
         $view = GeneralUtility::makeInstance(StandaloneView::class);
-        $view->assign('dataSource',$data);
+        $view->assign('dataSource',$input);
         $view->setTemplateSource('<html
             xmlns:f="http://typo3.org/ns/TYPO3/CMS/Fluid/ViewHelpers"
             xmlns:fc="SMS\FluidComponents\ViewHelpers"
             data-namespace-typo3-fluid="true"
             >' . $template);
 
-        self::assertSame($expected, $view->render());
+        $view->render();
+        self::assertSame($expected, $view->getRenderingContext()->getVariableProvider()->get('dataTarget'));
     }
 }
