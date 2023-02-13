2f1fe894ae22be120f002d1db60b861104eef544
Simon Praetorius <praetorius@sitegeist.de>
2023-02-13 12:18:50 +0100

    [TASK] Add format and processing to placeholder images

--- Classes/Domain/Model/PlaceholderImage.php
+++ Classes/Domain/Model/PlaceholderImage.php
@@ -3,11 +3,13 @@
 namespace SMS\FluidComponents\Domain\Model;
 
 use SMS\FluidComponents\Interfaces\ImageWithDimensions;
+use SMS\FluidComponents\Interfaces\ProcessableImage;
+use TYPO3\CMS\Core\Imaging\ImageManipulation\Area;
 
 /**
  * Data structure for a placeholder image to be passed to a component
  */
-class PlaceholderImage extends Image implements ImageWithDimensions
+class PlaceholderImage extends Image implements ImageWithDimensions, ProcessableImage
 {
     /**
      * Type of image to differentiate implementations in Fluid templates
@@ -31,15 +33,23 @@ class PlaceholderImage extends Image implements ImageWithDimensions
     protected $height = 0;
 
     /**
+     * Image format of the image
+     *
+     * @var string
+     */
+    protected $format = 'gif';
+
+    /**
      * Creates an image object for a placeholder image
      *
      * @param integer $width
      * @param integer $height
      */
-    public function __construct(int $width, int $height)
+    public function __construct(int $width, int $height, string $format = 'gif')
     {
         $this->width = $width;
         $this->height = $height;
+        $this->format = $format;
     }
 
     public function getWidth(): int
@@ -52,8 +62,22 @@ class PlaceholderImage extends Image implements ImageWithDimensions
         return $this->height;
     }
 
+    public function getFormat(): string
+    {
+        return $this->format;
+    }
+
     public function getPublicUrl(): string
     {
-        return 'https://via.placeholder.com/' . $this->width . 'x' . $this->height;
+        return 'https://via.placeholder.com/' . $this->width . 'x' . $this->height . '.' . $this->format;
+    }
+
+    public function process(int $width, int $height, string $format, Area $cropArea): ProcessableImage
+    {
+        return new PlaceholderImage(
+            $cropArea->getWidth() * $width,
+            $cropArea->getHeight() * $height,
+            $format
+        );
     }
 }
