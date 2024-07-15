87b2cc5f73a35ebdb8cada02c733c901bf043ddc
Ulrich Mathes <mathes@sitegeist.de>
2024-07-15 18:55:45 +0200

    [v12] match PHP and TYPO3 dependencies from composer to ext_emconf

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,8 +10,8 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.4.0-13.4.99',
-            'php' => '8.2.0-8.2.99'
+            'typo3' => '12.4.0-13.9.99',
+            'php' => '8.2.0-8.3.99'
         ],
         'conflicts' => [
         ],
